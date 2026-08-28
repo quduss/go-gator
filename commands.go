3f471998011a8c296b5fda923a897252c130fa26
quduss <qudshuaib@gmail.com>
2026-08-28 12:18:58 +0100

    Implement a method on the commands struct that runs a given command with the provided state if it exists

--- commands.go
+++ commands.go
@@ -10,3 +10,11 @@ type commands struct {
 func (c *commands) register(name string, f func(*state, command) error) {
 	c.registeredCommands[name] = f
 }
+
+func (c *commands) run(s *state, cmd command) error {
+	f, ok := c.registeredCommands[cmd.Name]
+	if !ok {
+		return errors.New("command not found")
+	}
+	return f(s, cmd)
+}
