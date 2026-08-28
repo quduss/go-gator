1a70f3731b13b9c01f67eca363b7006f39613f8b
quduss <qudshuaib@gmail.com>
2026-08-28 12:04:48 +0100

    Implement a method that registers a new handler function for a command name on the commands struct

--- commands.go
+++ commands.go
@@ -6,3 +6,7 @@ type command struct {
 type commands struct {
 	registeredCommands map[string]func(*state, command) error
 }
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.registeredCommands[name] = f
+}
