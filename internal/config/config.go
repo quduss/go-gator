9081e7f527a392c94fc1e52a37838b557a4662ba
quduss <qudshuaib@gmail.com>
2026-08-28 07:45:16 +0100

    Write a function that decodes the config file into a  Config struct

--- internal/config/config.go
+++ internal/config/config.go
@@ -16,3 +16,25 @@ func getConfigFilePath() (string, error) {
 	fullPath := filepath.Join(home, configFileName)
 	return fullPath, nil
 }
+
+func Read() (Config, error) {
+	fullPath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+
+	file, err := os.Open(fullPath)
+	if err != nil {
+		return Config{}, err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	cfg := Config{}
+	err = decoder.Decode(&cfg)
+	if err != nil {
+		return Config{}, err
+	}
+
+	return cfg, nil
+}
