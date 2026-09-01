a38df607d4e2d649484fc31ae117628536430d76
quduss <qudshuaib@gmail.com>
2026-09-01 15:24:37 +0100

    Update commands registry with the register command and its handler

--- main.go
+++ main.go
@@ -36,6 +36,7 @@ func main() {
 		registeredCommands: make(map[string]func(*state, command) error),
 	}
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
