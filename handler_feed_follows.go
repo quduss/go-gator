b7f324273e806499a0bbdede06f2cdb53d99d323
quduss <qudshuaib@gmail.com>
2026-09-05 15:38:26 +0100

    Update signatures of handlerFollow, handlerListFeedFollows and handlerAddFeed to include the User argument

--- handler_feed_follows.go
+++ handler_feed_follows.go
@@ -9,11 +9,7 @@ import (
 	"github.com/quduss/go-gator/internal/database"
 )
 
-func handlerFollow(s *state, cmd command) error {
-	user, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
-	if err != nil {
-		return err
-	}
+func handlerFollow(s *state, cmd command, user database.User) error {
 
 	if len(cmd.Args) != 1 {
 		return fmt.Errorf("usage: %s <feed_url>", cmd.Name)
@@ -37,11 +33,7 @@ func handlerFollow(s *state, cmd command) error {
 	return nil
 }
 
-func handlerListFeedFollows(s *state, cmd command) error {
-	user, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
-	if err != nil {
-		return err
-	}
+func handlerListFeedFollows(s *state, cmd command, user database.User) error {
 
 	feedFollows, err := s.db.GetFeedFollowsForUser(context.Background(), user.ID)
 	if err != nil {
