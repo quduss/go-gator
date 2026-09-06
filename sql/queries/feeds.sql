2e8c25d1e72ea3e1bd595d1e081e3f5c5c653b72
quduss <qudshuaib@gmail.com>
2026-09-06 17:45:36 +0100

    Write a query that returns the next feed we should fetch posts from

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -16,3 +16,8 @@ SET last_fetched_at = NOW(),
 updated_at = NOW()
 WHERE id = $1
 RETURNING *;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
